64d55d17b6577c1d0f82ac8acf9cf07627a65241
stripodi <simone.tripodi@terradue.com>
2026-02-10 17:29:14 +0100

    feat: defined initial CLI

--- src/osc_client/cli.py
+++ src/osc_client/cli.py
@@ -12,29 +12,63 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from pathlib import Path
+
 import click
 
 @click.group()
-def main():
+@click.argument(
+    'source',
+    type=click.Path(
+        path_type=Path,
+        exists=True,
+        readable=True,
+        resolve_path=True
+    ),
+    required=True
+)
+@click.option(
+    '--output',
+    type=click.Path(
+        path_type=Path
+    ),
+    required=False,
+    help="The output file path"
+)
+@click.pass_context
+def main(
+    ctx,
+    source: Path,
+    output: Path
+):
+    ctx.ensure_object(dict)
+    ctx.obj["source"] = source
+
+    output.parent.mkdir(
+        parents=True, exist_ok=True
+    )
+    ctx.obj["output"] = output
+
+
+@main.command(
+    context_settings={'show_default': True}
+)
+@click.pass_context
+def workflow(ctx):
     pass
 
 
 @main.command(
     context_settings={'show_default': True}
 )
-def workflow():
+@click.pass_context
+def experiment(ctx):
     pass
 
 
 @main.command(
     context_settings={'show_default': True}
 )
-def experiment():
-    pass
-
-
-@main.command(
-    context_settings={'show_default': True}
-)
-def products():
+@click.pass_context
+def products(ctx):
     pass
